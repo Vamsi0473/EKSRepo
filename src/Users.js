6750d5ba1e3b3c5d4eb48f1910175d1df5b2224e
Vamsi0473 <lakshmipuramvamsikrishna@gmail.com>
2021-04-12 17:01:47 -0400

    bug fix

--- src/Users.js
+++ src/Users.js
@@ -1,34 +1,34 @@
 import React from 'react'
 import AWS from 'aws-sdk'
 function Users() {
-    const BUCKET_NAME = "vireksbucket"
-    AWS.config.update({
-        accessKeyId: "AKIA3RTTNEZTEPI67TVK",
-        secretAccessKey: "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"
-      })
-      const s3 = new AWS.S3({
-        apiVersion: '2006-03-01',
-        params: {Bucket: BUCKET_NAME}
-      });
-      let get_keys_params = {
-        Bucket: BUCKET_NAME
-      }
-      let get_keys_params = {
-        Bucket: BUCKET_NAME,
-        Key: "User1.json"
-      }
-    //   s3.getObject(get_pic_params, function(err, data) {
-    //     if (err) {
-    //       console.log('There was an error getting a pic: ' + err.message);
-    //     } else {
-    //       console.log(data.Body.toString('utf-8'));
-    //     }
-    //   });
-      s3.listObjectsV2(get_pic_params, (err, data) => {
-        if (err) throw err;
-        console.log(data.Contents);
-       });
-//     const AWS = require('aws-sdk/global');
+//     const BUCKET_NAME = "vireksbucket"
+//     AWS.config.update({
+//         accessKeyId: "AKIA3RTTNEZTEPI67TVK",
+//         secretAccessKey: "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"
+//       })
+//       const s3 = new AWS.S3({
+//         apiVersion: '2006-03-01',
+//         params: {Bucket: BUCKET_NAME}
+//       });
+//       let get_keys_params = {
+//         Bucket: BUCKET_NAME
+//       }
+//       let get_keys_params = {
+//         Bucket: BUCKET_NAME,
+//         Key: "User1.json"
+//       }
+//     //   s3.getObject(get_pic_params, function(err, data) {
+//     //     if (err) {
+//     //       console.log('There was an error getting a pic: ' + err.message);
+//     //     } else {
+//     //       console.log(data.Body.toString('utf-8'));
+//     //     }
+//     //   });
+//       s3.listObjectsV2(get_pic_params, (err, data) => {
+//         if (err) throw err;
+//         console.log(data.Contents);
+//        });
+// //     const AWS = require('aws-sdk/global');
 
 //  const S3 = require('aws-sdk/clients/s3');
 
